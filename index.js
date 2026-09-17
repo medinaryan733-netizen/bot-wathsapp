b40ecc799e21c2f8ffefcb0ab4a27cb43cbeb328
medinaryan733-netizen <medinaryan733@gmail.com>
2026-09-17 15:09:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -31,4 +31,4 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-   console.log("Bot corriendo en puerto " + PORT);
+   console.log("Bot corriendo en puerto" + PORT);
