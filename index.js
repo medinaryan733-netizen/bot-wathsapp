82ffce4f9bfe4525769444447fed385f4bb53799
medinaryan733-netizen <medinaryan733@gmail.com>
2026-09-25 09:49:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ puppeteer: {
         '--disable-software-rasterizer',
         '--disable-extensions'
     ]
-}
+},
         // ESTE ES EL DISFRAZ CLAVE:
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     }
