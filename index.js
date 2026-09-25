4b5631d9673645e585fa730249fd711ad22eea30
medinaryan733-netizen <medinaryan733@gmail.com>
2026-09-25 01:16:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -156,15 +156,21 @@ app.post('/api/servicios/editar', async (req, res) => {
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    puppeteer: {
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-gpu',
-'--disable-software-rasterizer',
-'--disable-dev-shm-usage'
+puppeteer: {
+    headless: true,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // (Pruébalo ahora con este o sin este, pero asegúrate de tener los de abajo)
+        '--disable-gpu',
+        '--disable-software-rasterizer',
+        '--disable-extensions'
+    ]
+}
         ],
         // ESTE ES EL DISFRAZ CLAVE:
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
