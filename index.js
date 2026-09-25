5d321a9d8c29b9f0cc8488df6a79a60f18cb79df
medinaryan733-netizen <medinaryan733@gmail.com>
2026-09-25 01:03:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -162,7 +162,9 @@ const client = new Client({
             '--no-sandbox',
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
-            '--single-process'
+            '--disable-gpu',
+'--disable-software-rasterizer',
+'--disable-dev-shm-usage'
         ],
         // ESTE ES EL DISFRAZ CLAVE:
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
